2f222f223f834967ebbb6ad48becb5f9f248b45e
mohamadp91 <69380537+mohamadp91@users.noreply.github.com>
2021-04-30 01:22:45 +0430

    epub reader (#2)
    * Use epubjs library to read epub books
    
    * Render a epub book
    
    * Some changes
    
    * Optimize font size

--- src/index.js
+++ src/index.js
@@ -1,15 +1,63 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import ReactDOM from "react-dom"
+import ePub from "epubjs"
+import styled from "styled-components"
+import { Button } from "@material-ui/core"
+import {
+	NavigateNext as Next,
+	NavigateBefore as Prev,
+} from "@material-ui/icons"
+
 import { GlobalStyle } from "./style"
+import eBook from "./assets/larsson-stieg-luftslottet-som-sprängdes.epub"
+
+const ButtonContainerStyled = styled.div`
+	display: flex;
+	justify-content: space-between;
+	order: 2;
+	margin-top: 10px;
+`
 
 const App = () => {
-	return <div className="App"></div>
+	const [rendition, setRendition] = useState(null)
+
+	useEffect(() => {
+		const book = ePub(eBook)
+		const renditionBook = book.renderTo("root", {
+			width: 400,
+			height: 800,
+			flow: "paginated",
+		})
+		// eslint-disable-next-line
+		renditionBook.themes.default({ ".calibre": { "font-size": "1.5em" } })
+		renditionBook.display()
+		setRendition(renditionBook)
+	}, [])
+
+	const prevPage = () => {
+		rendition.prev()
+	}
+
+	const nextPage = () => {
+		rendition.next()
+	}
+
+	return (
+		<ButtonContainerStyled className="app">
+			<Button onClick={prevPage} variant="contained" color="primary">
+				<Prev />
+			</Button>
+			<Button onClick={nextPage} variant="contained" color="primary">
+				<Next />
+			</Button>
+		</ButtonContainerStyled>
+	)
 }
 
 ReactDOM.render(
-	<React.StrictMode>
+	<React.Fragment>
 		<GlobalStyle />
 		<App />
-	</React.StrictMode>,
+	</React.Fragment>,
 	document.getElementById("root")
 )
--- src/style/GlobalStyle.js
+++ src/style/GlobalStyle.js
@@ -6,18 +6,27 @@ export const GlobalStyle = createGlobalStyle`
   }
   
   body {
-  margin: 0;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
         'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
          sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+		text-align: left;
+		margin-left: 40px;
   }
 
    code {
     font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
          monospace;
 }
+	 #root{
+		 display: inline-flex;
+     flex-direction: column;
+	 }
+	 .epub-container{
+		 background-color: #F6EA7BFF;
+		 order: 1;
+	 }
 `
 
 export default GlobalStyle
